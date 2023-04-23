48fc0b1a5a375519f38d53d5658bda9c3c78527f
crhisgbibon <admin@crhisgbibon.com>
2023-04-23 21:34:34 +0100

    x

--- resources/views/map2.blade.php
+++ resources/views/map2.blade.php
@@ -3,6 +3,7 @@
     <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="icon" href="favicon.ico">
       <title>World</title>
       @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/vh.js', 'resources/js/map5.js'])
       <!-- Styles -->
